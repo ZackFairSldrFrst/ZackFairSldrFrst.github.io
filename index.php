df3bb02b28f3c0dd81dcf96959298053a740ba74
Alvin <alvin.hartono@gmail.com>
2018-08-24 13:16:58 -0700

    fixed php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 echo "Here are our files";
-$path = ".";
-$dh = opendir(/Users/alvin/Documents/GitHub/a-hartono.github.io);
+$path = "/Users/alvin/Documents/GitHub/a-hartono.github.io";
+$dh = opendir($path);
 $i=1;
 while (($file = readdir($dh)) !== false) {
-    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+    if($file != "/Users/alvin/Documents/GitHub/a-hartono.github.io" && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
         echo "<a href='$path/$file'>$file</a><br /><br />";
         $i++;
     }
